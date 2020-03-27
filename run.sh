744999ca1047886c072b9d37879d9e6238a676f6
Your Name <you@example.com>
2020-03-27 22:44:04 +0100

    changes in script

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
-hdfs dfs -rm -r output
-yarn jar jar/WordCount.jar MapReduce input output 
+hdfs dfs -rm -r -f output
+hdfs dfs -mkdir -p input
+hdfs dfs -copyFromLocal input/input.txt input
+yarn jar target/SampleMapReduce-1.0-SNAPSHOT.jar MapReduce input output 
