17a608cf6a2ed5d35208d309c9337203a68e20a7
Your Name <you@example.com>
2020-03-27 22:56:42 +0100

    runkerb.sh

--- runkerb.sh
+++ runkerb.sh
@@ -1,7 +1,8 @@
-hdfs dfs -rm -r output
+hdfs dfs -rm -r -f output
 HADOOP_CLIENT=/usr/hdp/current/hadoop-client
 CONF=/etc/hadoop/conf
 HDP_VERSION=3.1.0.0-78
-KERB_PRINCIPAL=user1@CENTOS.COM.REALM
-KERB_KEYTAB=/home/user1/user1.keytab
-java -Dhdp.version=$HDP_VERSION -cp jar/WordCount.jar:$HADOOP_CLIENT/*:$HADOOP_CLIENT/client/*:$CONF MapReduce input output $KERB_PRINCIPAL $KERB_KEYTAB
+KERB_PRINCIPAL=bench@CENTOS.COM.REALM
+KERB_KEYTAB=$HOME/bin/security/bench.keytab
+#java -Dhdp.version=$HDP_VERSION -cp target/SampleMapReduce-1.0-SNAPSHOT.jar:$HADOOP_CLIENT/*:$HADOOP_CLIENT/client/*:$CONF MapReduce input output $KERB_PRINCIPAL $KERB_KEYTAB
+yarn jar target/SampleMapReduce-1.0-SNAPSHOT.jar MapReduce input output $KERB_PRINCIPAL $KERB_KEYTAB
